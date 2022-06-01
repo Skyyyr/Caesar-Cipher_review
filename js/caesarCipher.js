039c53c55f41bb97c70ce1298c614b96e34dfe44
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-06-01 18:42:35 -0400

    added JS and py

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,41 @@
-exports.caesarCipher = function() {
+// exports.caesarCipher = function(string, shiftAmount) {
+function caesarCipher(string, shiftAmount) {
+    let alphabet="abcdefghijklmnopqrstuvwxyz"
+    let alphabetUpper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let answer=""
+    for (let char of string){
+        if (alphabet.includes(char)) {
+            let shiftIndex=alphabet.indexOf(char)+shiftAmount
+            if (shiftIndex>25) {
+                answer+=alphabet[(shiftIndex-26)]
+            }
+            else if (shiftIndex<0) {
+                answer+=alphabet[(26-Math.abs(shiftIndex))]       
+            }
+            else {
+                answer+=alphabet[shiftIndex]
+            }
+        }
+        else if (alphabetUpper.includes(char)) {
+            let shiftIndex=alphabetUpper.indexOf(char)+shiftAmount
+            if (shiftIndex>26) {
+                answer+=alphabetUpper[(shiftIndex-26)]
+            }
+            else if (shiftIndex<0) {
+                answer+=alphabetUpper[(26-Math.abs(shiftIndex))]        
+            }
+            else {
+                answer+=alphabetUpper[shiftIndex]
+            }
+        }
+        else {
+            answer+=char
+        }
+    }
+    return answer
+}
 
-};
+// console.log(caesarCipher("Boy! What a string!", -5)) //=== "Wjt! Rcvo v nomdib!")
+// console.log(caesarCipher("Hello zach168! Yes here.", 5)) //=== "Mjqqt efhm168! Djx mjwj.")
+// console.log(caesarCipher("Hello Zach168! Yes here.", -5)) //=== "Czggj Uvxc168! Tzn czmz.")
+
--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,25 @@
 def caesar_cipher(string, shift_amount):
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    alphabet_up=alphabet.upper()
+    
+    answer = ""
+    for char in string:
+        if char in alphabet:
+            shift_index= alphabet.index(char)+shift_amount
+            if shift_index >26:
+                answer+=alphabet[(shift_index-26)]
+            else:
+                answer+=alphabet[shift_index]
+        elif char in alphabet_up:
+            shift_index= alphabet_up.index(char)+shift_amount
+            if shift_index >26:
+                answer+=alphabet_up[(shift_index-26)]
+            else:
+                answer+=alphabet_up[shift_index]
+        else:
+            answer+=char
+    return answer
+          
+# print(caesar_cipher("Boy! What a string!", -5)) # == "Wjt! Rcvo v nomdib!")
+# print(caesar_cipher("Hello zach168! Yes here.", 5)) # == "Mjqqt efhm168! Djx mjwj.")
+# print(caesar_cipher("Hello Zach168! Yes here.", -5)) # == "Czggj Uvxc168! Tzn czmz.")
